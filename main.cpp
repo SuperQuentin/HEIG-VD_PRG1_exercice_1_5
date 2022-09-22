19f91fb682f4695ac4e767bc650e93a80eb5726f
SuperQuentin <quentin.aellen@infrack.ch>
2022-09-22 09:35:04 +0200

    feat: add a TODO into for using input instead of declaring variable

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 
 int main() {
+    // TODO : Take input instead of declaring variable
     float distanceInKm = 17.5; // Echallens -> Yverdon HEIG-VD
     float trainTicketPrice = 4.6; // Trajet simple avec demi-tarif
     float carAvgConsumption = 3.3; // Liter / 100km Motorcycle CB 500 x 2017
@@ -14,8 +15,6 @@ int main() {
     const float carTravelGasCost = carTravelConsumption * gasPricePerLiter;
     const float carDepreciation = distanceInKm * carDepreciationCostByKm;
     const float carTravelTotalCost = carTravelGasCost + carDepreciation;
-
-
 
     cout << "Ton trajet a un cout de " << carTravelTotalCost << " frs en voiture / moto" << endl;
 
