c4f5c4ad8c1c4aef1315a983a7d8c2bd72b0a2e0
SuperQuentin <quentin.aellen@infrack.ch>
2022-09-22 09:29:21 +0200

    feat: initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    float distanceInKm = 17.5; // Echallens -> Yverdon HEIG-VD
+    float trainTicketPrice = 4.6; // Trajet simple avec demi-tarif
+    float carAvgConsumption = 3.3; // Liter / 100km Motorcycle CB 500 x 2017
+    float gasPricePerLiter = 1.94; // https://www.tcs.ch/fr/camping-voyages/informations-touristiques/bon-a-savoir/peages-frais/prix-essence-europe.php
+    float carDepreciationCostByKm = 0.4;
+
+    const float carTravelConsumption = distanceInKm * carAvgConsumption / 100;
+    const float carTravelGasCost = carTravelConsumption * gasPricePerLiter;
+    const float carDepreciation = distanceInKm * carDepreciationCostByKm;
+    const float carTravelTotalCost = carTravelGasCost + carDepreciation;
+
+
+
+    cout << "Ton trajet a un cout de " << carTravelTotalCost << " frs en voiture / moto" << endl;
+
+    return 0;
+}
